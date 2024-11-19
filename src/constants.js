bc088d5ccf312934a830cfa65fa574344bc0c094
Alexander Sanderson <67873187+Vexelior@users.noreply.github.com>
2024-11-18 17:08:29 -0800

    ```text chore: Update API URL constant in constants.js
    This commit updates the API URL constant in the constants.js file to use the commented out URL. The change ensures that the application uses the correct API endpoint for making requests.

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,5 @@
 export const API_URL = 'https://localhost:7164';
+//export const API_URL ='https://debt-tracker-api-management.azure-api.net';
 
 export const DEBT_CONTROLLER = API_URL + '/Debt';
 export const DEBT_HISTORY_CONTROLLER = API_URL + '/DebtPreviousAmount';
