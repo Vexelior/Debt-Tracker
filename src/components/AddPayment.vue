5d1d6aaf9073238974ef3ab1320d48881a9551bb
Alexander Sanderson <67873187+Vexelior@users.noreply.github.com>
2024-11-21 18:36:20 -0800

    chore: Update button type in EditDebt.vue and DebtList.vue components

---
+++ src/components/AddPayment.vue
@@ -0,0 +1,59 @@
+<template>
+    <div class="container">
+        <h1 class="my-4">Submit Payment</h1>
+        <form @submit.prevent="submitPayment">
+            <div class="mb-3">
+                <label for="amount" class="form-label">Amount</label>
+                <input type="number" class="form-control" id="amount" step=".01" v-model="amount" required
+                    placeholder="Enter amount" />
+            </div>
+            <div class="mb-3">
+                <label for="date" class="form-label">Date</label>
+                <input type="date" class="form-control" id="date" v-model="date" required />
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+import { PAYMENT_CONTROLLER } from '../constants';
+
+export default {
+    name: 'SubmitPayment',
+    data() {
+        return {
+            amount: '',
+            date: '',
+            debtId: '',
+        };
+    },
+    created() {
+        this.debtId = this.$route.params.id;
+    },
+    methods: {
+        async submitPayment() {
+            try {
+                const payload = {
+                    amount: this.amount,
+                    date: this.date,
+                    debtId: this.debtId,
+                };
+                console.log('Payment controller:', PAYMENT_CONTROLLER);
+                await axios.post(PAYMENT_CONTROLLER, payload);
+                this.$router.push({ path: `/debt/${this.debtId}` });
+            } catch (error) {
+                console.error('Error submitting payment:', error);
+            }
+        },
+        resetForm() {
+            this.payment = {
+                amount: '',
+                date: '',
+                method: '',
+            };
+        },
+    },
+};
+</script>
--- src/constants.js
+++ src/constants.js
@@ -6,3 +6,4 @@ export const DEBT_HISTORY_CONTROLLER = API_URL + '/DebtHistory';
 export const DEBT_PREVIOUS_AMOUNT = API_URL + '/DebtPreviousAmount';
 export const LOGGING_CONTROLLER = API_URL + '/Logging';
 export const ACCOUNT_CONTROLLER = API_URL + '/Account';
+export const PAYMENT_CONTROLLER = API_URL + '/Payment';
